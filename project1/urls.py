7dbdccf4aa25dd71471a618386e5be2f714e52ae
Abtin Firoozyar <abtinfiroozyar@gmail.com>
2024-06-06 18:32:27 +0330

    create new app for managing accounts

--- project1/urls.py
+++ project1/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('homescreen.urls')),
     path('', views.home),
+    path('accounts/', include('accounts.urls'))
     
     
 ]
